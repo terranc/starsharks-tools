250eef001d45fb5708ff7ef572bf9498b63365ae
terranc <wuchaoboy@gmail.com>
2022-03-27 15:02:27 +0800

    1 file modified

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -16,12 +16,12 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#"
-              >小助手钱包格式化</a
-            >
+            <router-link class="nav-link" to="/">小助手钱包格式化</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">钱包批量生成</a>
+            <router-link class="nav-link" to="/address_batch_creator"
+              >钱包批量生成</router-link
+            >
           </li>
         </ul>
       </div>
