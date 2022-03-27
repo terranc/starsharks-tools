c97ed8168757ab38093dfc9ea21c2bdc9dd0de87
terranc <wuchaoboy@gmail.com>
2022-03-27 15:00:37 +0800

    1 file modified, 3 added, 3 deleted

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
+const { createApp } = require('vue');
 import App from './App.vue';
-import routes from './routes.js';
+import createRouter from './router';
 
-const router = createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  // history: VueRouter.createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
-});
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+createApp(App).use(createRouter()).mount('#app');
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import {
+  createRouter,
+  createWebHistory,
+} from 'vue-router/dist/vue-router.esm-bundler';
+
+export default () =>
+  createRouter({
+    // history: createWebHistory(),
+    routes: [
+      {
+        path: '/',
+        name: 'AddressFormator',
+        component: () => import('./views/AddressFormator.vue'),
+      },
+      {
+        path: '/address_batch_creator',
+        name: 'AddressBatchCreator',
+        component: () => import('./views/AddressBatchCreator.vue'),
+      },
+    ],
+  });
